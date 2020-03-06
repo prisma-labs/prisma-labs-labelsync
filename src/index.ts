5432362a97ef1752e1fa877d7fa39cbc79ed973d
Jason Kuhrt <jasonkuhrt@me.com>
2020-03-05 22:25:13 -0500

    chore: setup tweaks

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,57 @@
-import { configuration, make } from 'label-sync'
-
-import { github } from './repositories/github'
-import { prisma } from './repositories/prisma'
+import { configuration, label, make, repository } from 'label-sync'
 
 /**
- * Create a configuration instance by definining repositories
- * and their configurations.
+ * Default collection of label in a Github repository.
  */
-const config = configuration({
-  repositories: {
-    /**
-     * replace repository name with your own repositories
-     */
-    // 'emma-cli': github,
+export const github = repository({
+  strict: false,
+  labels: {
+    bug: label({
+      color: '#d73a4a',
+      description: "Something isn't working",
+    }),
+    documentation: label({
+      color: '#0075ca',
+      description: 'Improvements or additions to documentation',
+    }),
+    duplicate: label({
+      color: '#cfd3d7',
+      description: 'This issue or pull request already exists',
+    }),
+    enhancement: label({
+      color: '#a2eeef',
+      description: 'New feature or request',
+    }),
+    'good first issue': label({
+      color: '#7057ff',
+      description: 'Good for newcomers',
+    }),
+    'help wanted': label({
+      color: '#008672',
+      description: 'Extra attention is needed',
+    }),
+    invalid: label({
+      color: '#e4e669',
+      description: "This doesn't seem right",
+    }),
+    question: label({
+      color: '#d876e3',
+      description: 'Further information is requested',
+    }),
+    wontfix: label({
+      color: '#000000',
+      description: 'This will not be worked on',
+    }),
   },
 })
 
 /* Generates labelsync.yml into the root of repository. */
 make({
-  configs: [config],
+  configs: [
+    configuration({
+      repositories: {
+        'nexus-prisma': github,
+      },
+    }),
+  ],
 })
