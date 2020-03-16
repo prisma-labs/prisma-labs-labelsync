7183e8a83965b78f6b03167ae3475c775009e9bd
Jason Kuhrt <jasonkuhrt@me.com>
2020-03-16 14:46:12 -0400

    chore: remove nexus-prisma

--- labelsync.ts
+++ labelsync.ts
@@ -1,4 +1,4 @@
-import { commonLabels, note, repos, scope, theme, type } from './lib/core'
+import { commonLabels, repos, scope, theme, type } from './lib/core'
 
 //prettier-ignore
 export default repos([
@@ -34,29 +34,6 @@ export default repos([
       ...commonLabels,
       scope('extractor'),
       scope('md'),
-    ]
-  },
-  {
-    name: 'nexus-prisma',
-    destroyUnknownLabels: false,
-    labels: [
-      ...commonLabels,
-      type('epic',            theme.social,  ''),
-      type('meta',            theme.neutral, ''),
-      note('prisma1',                    'specific to the version of nexus-prisma based on Prisma 1'),
-      scope('architecture'),
-      scope('auth',                      'Relates to Prisma crud/model auth'),
-      scope('config'),
-      scope('crud'),
-      scope('distributed-data'),
-      scope('mapping'),
-      scope('projecting',                 'Exposing Prisma Models in the GQL API'),
-      scope('resolving',                  'Relates to implementing resolvers'),
-      scope('runtime-validation'),
-      scope('schema'),
-      scope('synchronized-projectors'),
-      scope('typegen',                    'Relates to typegen/codegen systems'),
-      scope('workflow'),
     ]
   },
 ])
